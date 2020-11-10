2a24462dd0714a40207f42fe04ff50e0bbc52e17
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2020-11-10 11:18:07 -0500

    lerp derive

--- crates/timescale/derive/src/lib.rs
+++ crates/timescale/derive/src/lib.rs
@@ -1,13 +1,72 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, ExprPath};
+use quote::{format_ident, quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned, ExprPath, Fields, Index, ItemStruct};
 
 #[proc_macro]
 pub fn load_csv(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ExprPath);
 
-    TokenStream::from(quote! {
+    TokenStream::from(quote! {})
+}
 
+#[proc_macro_derive(Lerp)]
+pub fn lerp_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemStruct);
+
+    let name = &input.ident;
+
+    TokenStream::from(match input.fields {
+        Fields::Named(fields) => {
+            let fields = fields.named.into_iter().map(|f| {
+                let name = f.ident;
+
+                quote! {
+                    #name: self.#name.lerp(other.#name, t)
+                }
+            });
+
+            quote! {
+                #[doc(hidden)]
+                #[allow(non_upper_case_globals, unused_attributes, unused_qualifications)]
+                const _: () = {
+                    extern crate serde as _serde;
+                    extern crate timescale as _timescale;
+                    use _timescale::Lerp;
+
+                    #[automatically_derived]
+                    impl Lerp<f64> for #name {
+                        fn lerp(self, other: Self, t: f64) -> Self {
+                            Self {
+                                #(#fields),*
+                            }
+                        }
+                    }
+                };
+            }
+        }
+        Fields::Unnamed(fields) => {
+            let fields = fields.unnamed.into_iter().enumerate().map(|(i, f)| {
+                let name = Index::from(i);
+
+                quote! {
+                    self.#name.lerp(other.#name, t)
+                }
+            });
+
+            quote! {
+                #[automatically_derived]
+                impl Lerp<f64> for #name {
+                    fn lerp(self, other: Self, t: f64) -> Self {
+                        Self (
+                            #(#fields),*
+                        )
+                    }
+                }
+            }
+        }
+        _ => quote_spanned! {input.span()=>
+            compile_error!("Structs must have fields");
+        },
     })
 }
 
--- crates/timescale/src/lib.rs
+++ crates/timescale/src/lib.rs
@@ -12,9 +12,8 @@ pub trait ToTimescale {
 }
 
 pub enum TimescaleData<D> {
-    Interpolation { low: D, high: D, percent: f64 },
-    HighSaturation(D),
-    LowSaturation(D),
+    Interpolation { prev: D, next: D, percent: f64 },
+    Saturation(D),
 }
 
 /// Trait to allow for linear interpolation through a static timescale lookup table for smoother lookups
@@ -29,8 +28,12 @@ pub trait TimescaleDataTable {
     /// if the time is outside of the table's range
     fn get_lerp(time: f64) -> Self::Datapoint {
         match Self::get(time) {
-            TimescaleData::HighSaturation(d) | TimescaleData::LowSaturation(d) => d,
-            TimescaleData::Interpolation { low, high, percent } => low.lerp(high, percent),
+            TimescaleData::Saturation(d) => d,
+            TimescaleData::Interpolation {
+                prev,
+                next,
+                percent,
+            } => prev.lerp(next, percent),
         }
     }
 }
@@ -54,15 +57,15 @@ mod test {
 
         fn get(time: f64) -> TimescaleData<Self::Datapoint> {
             match time {
-                _ if time <= 0.0 => TimescaleData::LowSaturation(0.0),
+                _ if time <= 0.0 => TimescaleData::Saturation(0.0),
                 // 0.0 => TimescaleData::Literal(0.0),
                 _ if time >= 0.0 && time < 0.5 => {
                     let time_high = 0.5;
                     let time_low = 0.0;
 
                     TimescaleData::Interpolation {
-                        high: 100.0,
-                        low: 0.0,
+                        next: 100.0,
+                        prev: 0.0,
                         percent: (time - time_low) / (time_high - time_low),
                     }
                 }
@@ -72,13 +75,13 @@ mod test {
                     let time_low = 0.5;
 
                     TimescaleData::Interpolation {
-                        high: 300.0,
-                        low: 100.0,
+                        next: 300.0,
+                        prev: 100.0,
                         percent: (time - time_low) / (time_high - time_low),
                     }
                 }
                 // 1.0 => TimescaleData::Literal(300.0),
-                _ if time >= 1.0 => TimescaleData::HighSaturation(300.0),
+                _ if time >= 1.0 => TimescaleData::Saturation(300.0),
                 _ => unreachable!(),
             }
         }
@@ -109,22 +112,31 @@ mod test {
     }
 
     struct DataTable2;
+
+    #[derive(Lerp)]
+    #[derive(PartialEq, Debug)]
     struct Data2(f64, f64);
+
+    // impl Lerp<f64> for Data2 {
+    //     fn lerp(self, other: Self, t: f64) -> Self {
+    //         Self(self.0.lerp(other.0, t), self.1.lerp(other.1, t))
+    //     }
+    // }
 
     impl TimescaleDataTable for DataTable2 {
         type Datapoint = Data2;
 
         fn get(time: f64) -> TimescaleData<Self::Datapoint> {
             match time {
-                _ if time <= 0.0 => TimescaleData::LowSaturation(0.0),
+                _ if time <= 0.0 => TimescaleData::Saturation(Data2(0.0, 0.0)),
                 // 0.0 => TimescaleData::Literal(0.0),
                 _ if time >= 0.0 && time < 0.5 => {
                     let time_high = 0.5;
                     let time_low = 0.0;
 
                     TimescaleData::Interpolation {
-                        high: 100.0,
-                        low: 0.0,
+                        next: Data2(100.0, -100.0),
+                        prev: Data2(0.0, 0.0),
                         percent: (time - time_low) / (time_high - time_low),
                     }
                 }
@@ -134,40 +146,39 @@ mod test {
                     let time_low = 0.5;
 
                     TimescaleData::Interpolation {
-                        high: 300.0,
-                        low: 100.0,
+                        next: Data2(300.0, -300.0),
+                        prev: Data2(100.0, -100.0),
                         percent: (time - time_low) / (time_high - time_low),
                     }
                 }
                 // 1.0 => TimescaleData::Literal(300.0),
-                _ if time >= 1.0 => TimescaleData::HighSaturation(300.0),
+                _ if time >= 1.0 => TimescaleData::Saturation(Data2(300.0, -300.0)),
                 _ => unreachable!(),
             }
         }
     }
 
-
     #[test]
     fn datatable_2_units() {
-        assert_eq!(DataTable::get_lerp(-0.1), 0.0);
+        assert_eq!(DataTable2::get_lerp(-0.1), Data2(0.0, 0.0));
         // assert_eq!(DataTable::get_quantized(-0.1), None);
 
-        assert_eq!(DataTable::get_lerp(0.0), 0.0);
+        assert_eq!(DataTable2::get_lerp(0.0), Data2(0.0, 0.0));
         // assert_eq!(DataTable::get_quantized(0.0), Some(0.0));
 
-        assert_eq!(DataTable::get_lerp(0.1), 20.0);
+        assert_eq!(DataTable2::get_lerp(0.1), Data2(20.0, -20.0));
         // assert_eq!(DataTable::get_quantized(0.1), None);
 
-        assert_eq!(DataTable::get_lerp(0.5), 100.0);
+        assert_eq!(DataTable2::get_lerp(0.5), Data2(100.0, -100.0));
         // assert_eq!(DataTable::get_quantized(0.5), Some(100.0));
 
-        assert_eq!(DataTable::get_lerp(0.6), 140.0);
+        assert_eq!(DataTable2::get_lerp(0.6), Data2(140.0, -140.0));
         // assert_eq!(DataTable::get_quantized(0.6), None);
 
-        assert_eq!(DataTable::get_lerp(1.0), 300.0);
+        assert_eq!(DataTable2::get_lerp(1.0), Data2(300.0, -300.0));
         // assert_eq!(DataTable::get_quantized(1.0), Some(300.0));
 
-        assert_eq!(DataTable::get_lerp(1.1), 300.0);
+        assert_eq!(DataTable2::get_lerp(1.1), Data2(300.0, -300.0));
         // assert_eq!(DataTable::get_quantized(1.1), None);
     }
 }
