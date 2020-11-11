45112bc1e70f2052a0412bcd436463b85d604875
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2020-11-11 01:08:17 -0500

    loads of derive

--- crates/timescale/derive/src/lib.rs
+++ crates/timescale/derive/src/lib.rs
@@ -1,71 +1,170 @@
+use std::path::PathBuf;
+
 use proc_macro::TokenStream;
 use quote::{format_ident, quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, ExprPath, Fields, Index, ItemStruct};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+    punctuated::Punctuated,
+    spanned::Spanned,
+    AttrStyle, Expr, ExprAssign, ExprLit, ExprPath, Fields, ItemStruct, Lit, LitStr, Path,
+    PathArguments, Token,
+};
 
-#[proc_macro]
-pub fn load_csv(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as ExprPath);
-
-    TokenStream::from(quote! {})
+#[derive(Default, Debug)]
+struct LoadCsvArguments {
+    st: Option<Path>,
+    file: Option<LitStr>,
 }
 
-#[proc_macro_derive(Lerp)]
-pub fn lerp_derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as ItemStruct);
+impl Parse for LoadCsvArguments {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let arguments: Punctuated<ExprAssign, Token![,]> =
+            input.parse_terminated(ExprAssign::parse)?;
 
-    let name = &input.ident;
+        Ok(arguments
+            .into_iter()
+            .try_fold(LoadCsvArguments::default(), |pre, expr| {
+                if let Expr::Path(ExprPath { path, .. } ) = *expr.left {
+                    let name = path.segments.iter().map(|seg| {
+                        seg.ident.to_string()
+                    }).collect::<Vec<_>>().join("::");
 
-    TokenStream::from(match input.fields {
-        Fields::Named(fields) => {
-            let fields = fields.named.into_iter().map(|f| {
-                let name = f.ident;
-
-                quote! {
-                    #name: self.#name.lerp(other.#name, t)
-                }
-            });
-
-            quote! {
-                #[doc(hidden)]
-                #[allow(non_upper_case_globals, unused_attributes, unused_qualifications)]
-                const _: () = {
-                    extern crate serde as _serde;
-                    extern crate timescale as _timescale;
-                    use _timescale::Lerp;
-
-                    #[automatically_derived]
-                    impl Lerp<f64> for #name {
-                        fn lerp(self, other: Self, t: f64) -> Self {
-                            Self {
-                                #(#fields),*
+                    match (name.as_str(), *expr.right) {
+                        ("st", Expr::Path(ExprPath { path , .. })) => {
+                            if pre.st.is_some() {
+                                Err(syn::Error::new(
+                                    path.span(),
+                                    "duplicate definition of field `st`",
+                                ))
+                            } else {
+                                Ok(LoadCsvArguments {
+                                    st: Some(path),
+                                    ..pre
+                                })
+                            }
+                        },
+                        ("file", Expr::Lit(ExprLit { lit: Lit::Str(str_lit), .. })) => {
+                            if pre.file.is_some() {
+                                Err(syn::Error::new(
+                                    str_lit.span(),
+                                    "duplicate definition of field `file`",
+                                ))
+                            } else {
+                                Ok(LoadCsvArguments {
+                                    file: Some(str_lit),
+                                    ..pre
+                                })
                             }
                         }
+                        (name, _) => Err(syn::Error::new(
+                            path.segments.span(),
+                            format!("unknown argument `{}`.\navailable arguments are `file` and `st`", name),
+                        ))
                     }
-                };
+                } else {
+                    Err(syn::Error::new(
+                        expr.span(),
+                        "invalid argument syntax.\nexpected syntax `argument_name = \"argument_value\"`",
+                    ))
+                }
+            })?)
+    }
+}
+
+#[proc_macro_derive(TimescaleDataTable, attributes(csv))]
+pub fn load_csv(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemStruct);
+
+    if Fields::Unit != input.fields {
+        return TokenStream::from(quote_spanned! {input.span()=>
+            compile_error!("The struct must be a unit struct");
+        });
+    }
+
+    let attributes = input
+        .attrs
+        .iter()
+        .filter(|attr| {
+            if let [segment] = attr.path.segments.iter().collect::<Vec<_>>().as_slice() {
+                attr.style == AttrStyle::Outer
+                    && segment.arguments == PathArguments::None
+                    && segment.ident.to_string() == "csv"
+            } else {
+                false
             }
-        }
-        Fields::Unnamed(fields) => {
-            let fields = fields.unnamed.into_iter().enumerate().map(|(i, f)| {
-                let name = Index::from(i);
+        })
+        .try_fold::<_, _, syn::Result<_>>(LoadCsvArguments::default(), |pre, a| {
+            let input = a.parse_args::<LoadCsvArguments>()?;
+
+            if let Some(st) = pre.st {
+                Err(syn::Error::new(
+                    st.span(),
+                    "duplicate definition of field `st`",
+                ))
+            } else if let Some(lit) = pre.file {
+                Err(syn::Error::new(
+                    lit.span(),
+                    "duplicate definition of field `file`",
+                ))
+            } else {
+                Ok(LoadCsvArguments {
+                    file: input.file.or(pre.file),
+                    st: input.st.or(pre.st),
+                })
+            }
+        });
+
+    TokenStream::from(match attributes {
+        Err(e) => e.to_compile_error(),
+        Ok(attributes) => match attributes {
+            LoadCsvArguments { file: None, .. } => {
+                quote_spanned! {input.ident.span()=>
+                    compile_error!("a path to load csv from using the attribute `#[csv(path = ...)]` must be provided");
+                }
+            }
+            LoadCsvArguments { st: None, .. } => {
+                quote_spanned! {input.ident.span()=>
+                    compile_error!("struct to deserialize the csv as using the attribute `#[csv(struct = ...)] must be provided`");
+                }
+            }
+            LoadCsvArguments {
+                file: Some(file),
+                st: Some(st),
+            } => {
+                let path = PathBuf::from(format!(
+                    "{}/{}",
+                    std::env::var("CARGO_MANIFEST_DIR")
+                        .expect("env var CARGO_MANIFEST_DIR missing"),
+                    file.value()
+                ));
+
+                if !path.exists() {
+                    let path = path.to_string_lossy();
+                    return TokenStream::from(quote_spanned! {file.span()=>
+                        compile_error!(concat!("File ", #path, " does not exist"));
+                    });
+                }
+
+                let data_table_struct = input.ident;
 
                 quote! {
-                    self.#name.lerp(other.#name, t)
-                }
-            });
+                    const _: () = {
+                        use timescale::TimescaleData;
 
-            quote! {
-                #[automatically_derived]
-                impl Lerp<f64> for #name {
-                    fn lerp(self, other: Self, t: f64) -> Self {
-                        Self (
-                            #(#fields),*
-                        )
-                    }
+                        #[automatically_derived]
+                        impl TimescaleDataTable for #data_table_struct {
+                            type Datapoint = #st;
+
+                            fn get(time: f64) -> TimescaleData<Self::Datapoint> {
+                                match time {
+                                   
+                                }
+                            }
+                        }
+                    };
                 }
             }
-        }
-        _ => quote_spanned! {input.span()=>
-            compile_error!("Structs must have fields");
         },
     })
 }
--- src/force_generator.rs
+++ src/force_generator.rs
@@ -1,3 +1,12 @@
-pub struct RocketEngine {
+use timescale::{Lerp, TimescaleDataTable};
 
+#[derive(Lerp)]
+pub struct RocketEngine {
+    thrust: f64,
 }
+
+// load_csv!(from "../csv/Estes_C6.csv" load RocketEngine);
+
+#[derive(TimescaleDataTable)]
+#[csv(file = "csv/Estes_C6.csv", st = RocketEngine)]
+pub struct EstesC6;
